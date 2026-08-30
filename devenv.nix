1aa6916067de9029a521584f32d6206802aa6e51
Oka Jelantik <okajelantikstdy@gmail.com>
2026-08-31 00:10:06 +0800

    updating devenv

--- devenv.nix
+++ devenv.nix
@@ -3,6 +3,18 @@
 {
   languages.javascript = {
     enable = true;
-    npm.enable = true;
+  };
+
+  env = {
+    NPM_CONFIG_FUND = "false";
+    NPM_CONFIG_AUDIT = "false";
+  };
+
+  git-hooks.hooks = {
+    prettier = {
+      enable = true;
+      entry = "${pkgs.prettier}/bin/prettier --write";
+      files = "\\.(js|jsx|ts|tsx|json|css|md|html)$";
+    };
   };
 }
